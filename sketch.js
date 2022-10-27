151e14b2236b975197cdd812de221846b62ad851
Larissa Celeste <larissa.celeste@sou.unifeob.edu.br>
2022-10-27 11:40:48 -0300

    versao da prof

--- sketch.js
+++ sketch.js
@@ -6,15 +6,15 @@ var arrow1;
 var PLAY = 2;
 var gamestate = PLAY;
 function preload() {
+  bg=loadImage("assets/background.png")
   downarrow = loadImage("assets/down.png");
   uparrow = loadImage("assets/up.png");
   leftarrow = loadImage("assets/left.png");
   rightarrow = loadImage("assets/right.png");
-  bfRight = loadImage("assets/BfRight.png")
-  bfLeft = loadImage("assets/BfLeft.png")
-  bfDown = loadImage("assets/BfDown.png")
-  bfUp = loadImage("assets/BfUp.png")
-  bfIdle = loadImage("assets/BfIdle.png")
+bfDown=loadAnimation("assets/Down/bfDown0.png","assets/Down/bfDown1.png","assets/Down/bfDown2.png","assets/Down/bfDown3.png","assets/Down/bfDown5.png")
+bfRight=loadAnimation("assets/Right/bfRight0.png","assets/Right/bfRight1.png","assets/Right/bfRight2.png","assets/Right/bfRight3.png","assets/Right/bfRight5.png")
+bfLeft=loadAnimation("assets/Left/bfLeft0.png","assets/Left/bfLeft1.png","assets/Left/bfLeft2.png","assets/Left/bfLeft3.png","assets/Left/bfLeft5.png")
+bfUp=loadAnimation("assets/Up/bfUp0.png","assets/Up/bfUp1.png","assets/Up/bfUp2.png","assets/Up/bfUp3.png","assets/Up/bfUp5.png")
 }
 
 function setup() {
@@ -46,12 +46,19 @@ function setup() {
   arrowPlayerRIGHT.scale=0.2
   arrowPlayerRIGHT.visible=false
   
+  bf =createSprite(width/2+250,height-200,50,50)
+ 
+  bf.addAnimation('d',bfDown);
+  bf.addAnimation('l',bfLeft);
+  bf.addAnimation('r',bfRight);
+  bf.addAnimation('u',bfUp);
+
   
 }
 
 function draw() {
   background(0);
-    
+    image(bg,0,0,windowWidth, windowHeight)
   
 
 if(gamestate=== PLAY){
@@ -67,19 +74,7 @@ if(gamestate=== PLAY){
       console.log("foi")
       score+=1925
       arrowupGroup[i].destroy()
-      bfUp=createImg("assets/bfUp.png")
-      bfUp.position(width/2+350,height/2)
-     bfUp.size(250,350)
-     setTimeout(()=>{
-      bfUp.remove()
-     },900)
-     setTimeout(()=>{
-      bfIdle.remove()
-     bfRight.remove()
-       bfDown.remove()
-       bfLeft.remove()
-     },100)
-     
+     bf.changeAnimation("u")
 
      }
     }
@@ -92,19 +87,7 @@ if(gamestate=== PLAY){
         console.log("foi")
         score+=1843
         arrowrightGroup[i].destroy()
-        bfRight=createImg("assets/bfRight.png")
-        bfRight.position(width/2+350,height/2)
-       bfRight.size(250,350)
-       setTimeout(()=>{
-        bfRight.remove()
-       },900)
-       setTimeout(()=>{
-        bfIdle.remove()
-       bfUp.remove()
-         bfDown.remove()
-         bfLeft.remove()
-       },100)
-  
+        bf.changeAnimation("r")
        }
       }
      }
@@ -116,19 +99,7 @@ if(gamestate=== PLAY){
         console.log("foi")
         score+=434
         arrowleftGroup[i].destroy()
-        bfLeft=createImg("assets/bfLeft.png")
-        bfLeft.position(width/2+350,height/2)
-       bfLeft.size(250,350)
-       setTimeout(()=>{
-        bfLeft.remove()
-       },900)
-       setTimeout(()=>{
-        bfIdle.remove()
-       bfRight.remove()
-         bfDown.remove()
-         bfUp.remove()
-       },100)
-  
+        bf.changeAnimation("l")
        }
       }
      }
@@ -140,19 +111,7 @@ if(gamestate=== PLAY){
         console.log("foi")
         score+=172
         arrowdownGroup[i].destroy()
-        bfDown=createImg("assets/bfDown.png")
-        bfDown.position(width/2+350,height/2)
-       bfDown.size(250,350)
-       setTimeout(()=>{
-        bfDown.remove()
-       },900)
-       setTimeout(()=>{
-        bfIdle.remove()
-       bfRight.remove()
-         bfUp.remove()
-         bfLeft.remove()
-       },100)
-  
+        bf.changeAnimation("d")
        }
       }
      }
@@ -166,23 +125,6 @@ if(gamestate=== PLAY){
   HandleArrowDown()
   }
   
-  bfIdle = createImg("assets/BfIdle.png")
-  bfIdle.position(width/2+350,height/2)
-  bfIdle.size(250,350)
-
-  bfIdle.depth = bfLeft.depth 
-  bfIdle.depth +=1 
-
-  bfIdle.depth = bfRight.depth 
-  bfIdle.depth +=1 
-
-  bfIdle.depth = bfUp.depth 
-  bfIdle.depth +=1 
-
-  bfIdle.depth = bfDown.depth 
-  bfIdle.depth +=1 
-
-  
   drawSprites();
  
 }
@@ -192,7 +134,7 @@ function HandleArrowLeft() {
   if (frameCount % 100 === 0) {
     arrowleft = createSprite(width / 2 -200, height, 40, 40);
     arrowleft.addImage("left", leftarrow); 
-    arrowleft.velocityY = random(-9,-30)
+    arrowleft.velocityY = random(-9,-15)
     arrowleft.scale=0.04
     arrowleft.setCollider("rectangle",0,0,10,10)
     arrowleft.lifetime=200
@@ -203,7 +145,7 @@ function HandleArrowDown() {
   if (frameCount % 100 === 0) {
     arrowdown = createSprite(width / 2-50, height, 40, 40);
     arrowdown.addImage("down", downarrow);
-    arrowdown.velocityY = random(-9,-30)
+    arrowdown.velocityY = random(-9,-15)
     
     arrowdown.scale=0.6
     arrowdown.lifetime=200
@@ -215,7 +157,7 @@ function HandleArrowUp() {
     arrowup = createSprite(width / 2 + 50, height, 40, 40);
     arrowup.addImage("down", uparrow);
     arrowup.scale=0.4
-    arrowup.velocityY = random(-9,-30)
+    arrowup.velocityY = random(-9,-15)
 
     arrowup.lifetime=200
     arrowupGroup.add(arrowup);
@@ -226,7 +168,7 @@ function HandleArrowRight() {
   if (frameCount % 100 === 0) {
     arrowright = createSprite(width / 2 +200, height, 40, 40);
     arrowright.addImage("right", rightarrow);    
-    arrowright.velocityY = random(-9,-30)
+    arrowright.velocityY = random(-9,-15)
     arrowright.scale=0.2
  
     arrowright.lifetime=200
